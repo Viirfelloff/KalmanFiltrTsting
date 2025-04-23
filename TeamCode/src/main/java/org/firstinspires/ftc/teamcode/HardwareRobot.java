bb8fad72ee7984a24af0e2a52ada9a6ad351d8a8
Your Name <you@example.com>
2025-04-23 16:20:19 -0400

    thing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareRobot.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.Motor.Encoder;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.subsystems.GoBildaPinpointDriver;
+
+public class HardwareRobot {
+    public final MotorEx leftFront;
+    public final MotorEx rightFront;
+    public final MotorEx leftBack;
+    public final MotorEx rightBack;
+
+
+
+    public final WebcamName cameraName;
+
+    public final Servo claw;
+    public final Servo elbow;
+    public final Servo clawrotation;
+
+    public HardwareRobot(HardwareMap hardwareMap) {
+
+        ////////////
+        // WHEELS //
+        ////////////
+        leftFront = new MotorEx(hardwareMap, "leftFront", Motor.GoBILDA.RPM_312);
+        rightFront = new MotorEx(hardwareMap, "rightFront", Motor.GoBILDA.RPM_312);
+        leftBack = new MotorEx(hardwareMap, "leftBack", Motor.GoBILDA.RPM_312);
+        rightBack = new MotorEx(hardwareMap, "rightBack", Motor.GoBILDA.RPM_312);
+
+        leftFront.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftFront.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        leftFront.setRunMode(Motor.RunMode.RawPower);
+        rightFront.setRunMode(Motor.RunMode.RawPower);
+        leftBack.setRunMode(Motor.RunMode.RawPower);
+        rightBack.setRunMode(Motor.RunMode.RawPower);
+
+        leftFront.setInverted(true);
+        rightFront.setInverted(true);
+        leftBack.setInverted(true);
+        rightBack.setInverted(true);
+
+        leftFront.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftFront.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+
+        /////////////
+        // SERVOS  //
+        /////////////
+        claw = hardwareMap.get(ServoImplEx.class, "Claw");
+        elbow = hardwareMap.get(ServoImplEx.class, "Elbow");
+        clawrotation = hardwareMap.get(ServoImplEx.class, "Claw Rotation");
+        ////////////
+        // CAMERA //
+        ////////////
+        cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+    }
+}
